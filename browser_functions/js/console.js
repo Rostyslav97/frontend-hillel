544df7585217a712dfa0011d698d4508862ac547
Rostyslav97 <malanyukrostik@gmail.com>
2025-05-29 11:00:52 +0300

    classwork 2

--- browser_functions/js/console.js
+++ browser_functions/js/console.js
@@ -6,23 +6,29 @@
 // console.log() Виводить повідомлення в консоль.
 console.log('Hello console');
 console.log(1, 2, 3);
+console.log(2 + 2);
 
 
 // console.error() Виводить помилкове повідомлення в консоль.
-// console.error('Hello Error');
+console.error('Hello Error');
 
 
 // console.warn() Виводить попереджувальне повідомлення в консоль.
-// console.warn('Hello warning')
+console.warn('Hello warning')
 
 
 // console.info() Виводить інформаційне повідомлення в консоль.
-// console.info('Hello Info')
+console.info('Hello Info')
 
 
 // console.group() і console.groupEnd()
 
 // console.group("Перша група");
+// console.log("Повідомлення 1 у першій групі");
+// console.log("Повідомлення 2 у першій групі");
+// console.groupEnd();
+
+// console.groupCollapsed("Перша група");
 // console.log("Повідомлення 1 у першій групі");
 // console.log("Повідомлення 2 у першій групі");
 // console.groupEnd();
@@ -40,11 +46,11 @@ console.log(1, 2, 3);
 
 // console.time() і console.timeEnd() Дозволяють виміряти час виконання між цими двома викликами.
 
-// console.time("timer");
-//
-// // Тут розташовується код, тривалість виконання якого ви хочете виміряти
-// for(let i = 0; i < 1000000; i++) {
-//     let square = i * i;
-// }
-//
-// console.timeEnd("timer");
+console.time("timer");
+
+// Тут розташовується код, тривалість виконання якого ви хочете виміряти
+for(let i = 0; i < 1000000; i++) {
+    let square = i * i;
+}
+
+console.timeEnd("timer");
