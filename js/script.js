773b5905adb1ac49739fc3e292ff0855429d0d4d
Rostyslav97 <malanyukrostik@gmail.com>
2025-06-17 16:38:36 +0300

    add hw-14

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const num1 = +prompt('Please enter first number');
+const num2 = +prompt('Please enter second number');
+const num3 = +prompt('Please enter third number');
+
+const average = (num1 + num2 + num3) / 3;
+
+alert(`Середнє арифметичне: ${average}`);
