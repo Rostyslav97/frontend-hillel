499c9317687f24938b3457d454c3f1777dd948bb
Rostyslav97 <malanyukrostik@gmail.com>
2025-08-23 16:06:31 +0300

    add hw-33

---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const toggleBtn = document.getElementById("toggle-theme");
+
+toggleBtn.addEventListener("click", function() {
+    document.body.classList.toggle("dark-theme");
+});
