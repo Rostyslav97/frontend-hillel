5e05733e0af5c7b8e459ab500f5c08cb680e38d3
Rostyslav97 <malanyukrostik@gmail.com>
2025-10-06 15:13:31 +0300

    add cl-30

---
+++ lesson-files/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import  Header from "./components/Header"
+import  Footer from "./components/Footer"
+
+function App() {
+  return (
+      <>
+        <Header />
+
+        <div className='container'>
+            <Footer />
+        </div>
+      </>
+  )
+}
+
+export default App
