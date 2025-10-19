ccf43136764cb23ac4c4bc89c7e03debc7e13d02
Rostyslav97 <malanyukrostik@gmail.com>
2025-10-19 12:32:42 +0300

    update hw-46

--- js/script.js
+++ js/script.js
@@ -1,27 +1,62 @@
 'use strict';
 
 class URLParser {
-    constructor(url = window.location.href) {
-        this.location = new URL(url);
+    constructor(url) {
+        this.url = url;
     }
 
     get protocol() {
-        return this.location.protocol;
+        const protocolEnd = this.url.indexOf(':');
+        return protocolEnd !== -1 ? this.url.substring(0, protocolEnd + 1) : '';
     }
 
     get hostname() {
-        return this.location.hostname;
+        let start = this.url.indexOf('//');
+        if (start !== -1) {
+            start += 2;
+        } else {
+            start = 0;
+        }
+
+        const end = this.url.indexOf('/', start);
+        const hostPart = end !== -1 ? this.url.substring(start, end) : this.url.substring(start);
+
+        // Відкидаємо порт, якщо є
+        const colonIndex = hostPart.indexOf(':');
+        return colonIndex !== -1 ? hostPart.substring(0, colonIndex) : hostPart;
     }
 
     get path() {
-        return this.location.pathname;
+        const pathStart = this.url.indexOf(this.hostname) + this.hostname.length;
+        const queryStart = this.url.indexOf('?', pathStart);
+        if (queryStart !== -1) {
+            return this.url.substring(pathStart, queryStart);
+        }
+        const hashStart = this.url.indexOf('#', pathStart);
+        if (hashStart !== -1) {
+            return this.url.substring(pathStart, hashStart);
+        }
+        return this.url.substring(pathStart);
     }
 
     get queryParams() {
+        const queryStart = this.url.indexOf('?');
+        if (queryStart === -1) return {};
+
+        const queryEnd = this.url.indexOf('#', queryStart);
+        const queryString = queryEnd !== -1
+            ? this.url.substring(queryStart + 1, queryEnd)
+            : this.url.substring(queryStart + 1);
+
+        const pairs = queryString.split('&');
         const params = {};
-        for (const [key, value] of this.location.searchParams.entries()) {
-            params[key] = value;
+
+        for (const pair of pairs) {
+            if (!pair) continue;
+            const [key, value = ''] = pair.split('=');
+            params[decodeURIComponent(key)] = decodeURIComponent(value);
         }
+
         return params;
     }
 }
